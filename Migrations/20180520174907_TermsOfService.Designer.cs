ff8087568c9058542e095515c927fcd4c0ea56ea
nielsladekarl <nielsladekarl@gmail.com>
2018-05-20 19:53:35 +0200

    Allow post and put user to send privacy policy acceptance

--- Migrations/20180520174907_TermsOfService.Designer.cs
+++ Migrations/20180520174907_TermsOfService.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Refundeo.Migrations
 {
     [DbContext(typeof(RefundeoDbContext))]
-    partial class RefundeoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180520174907_TermsOfService")]
+    partial class TermsOfService
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20180520174907_TermsOfService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Refundeo.Migrations
+{
+    public partial class TermsOfService : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptedTermsOfService",
+                table: "CustomerInformations",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TermsOfService",
+                table: "CustomerInformations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcceptedTermsOfService",
+                table: "CustomerInformations");
+
+            migrationBuilder.DropColumn(
+                name: "TermsOfService",
+                table: "CustomerInformations");
+        }
+    }
+}
