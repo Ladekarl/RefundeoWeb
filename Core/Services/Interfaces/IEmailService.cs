2345ed60a3f9e6ac12047a345a26b6072d21b20b
nielsladekarl <nielsladekarl@gmail.com>
2018-06-23 21:21:55 +0200

    sending vat form attachment

--- Core/Services/Interfaces/IEmailService.cs
+++ Core/Services/Interfaces/IEmailService.cs
@@ -1,11 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
 using Refundeo.Core.Data.Models;
 
 namespace Refundeo.Core.Services.Interfaces
 {
     public interface IEmailService
     {
-        Task SendMailAsync(string subject, string body, string receiverEmail);
+        Task SendMailAsync(string subject, string body, string receiverEmail, bool isHtml,
+            Attachment attachment)
         Task SendVATMailAsync(RefundCase refundCase, string receiverEmail);
     }
 }
