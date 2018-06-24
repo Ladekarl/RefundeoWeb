e9354b7320092ebc1c996526939818d67836b679
nielsladekarl <nielsladekarl@gmail.com>
2018-06-24 21:31:05 +0200

    add

--- Core/Services/EmailService.cs
+++ Core/Services/EmailService.cs
@@ -203,10 +203,6 @@ namespace Refundeo.Core.Services
                 html = output.GetStringBuilder();
             }
 
-//            var baseUrl = string.Format("{0}://{1}", context.HttpContext.Request.Scheme,
-//                context.HttpContext.Request.Host);
-//            var htmlForWkhtml = Regex.Replace(html.ToString(), "<head>",
-//                string.Format("<head><base href=\"{0}\" />", baseUrl), RegexOptions.IgnoreCase);
             return html.ToString();
         }
     }
