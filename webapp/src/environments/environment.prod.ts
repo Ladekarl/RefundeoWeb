6608d9afeff0c47a9cef716d84d9e8823fd413ec
nielsladekarl <nielsladekarl@gmail.com>
2018-07-10 15:08:38 +0200

    changed to point at app.refundeo.com

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     hmr: false,
-    apiBaseUrl: 'https://refundeo20180331121625.azurewebsites.net'
+    apiBaseUrl: 'https://app.refundeo.com'
 };
