e0525756ee34e1b3f58abeed58f87623c211a5ed
nielsladekarl <nielsladekarl@gmail.com>
2018-07-16 23:28:01 +0200

    Add retailers

--- webapp/src/app/services/merchantinfo.service.ts
+++ webapp/src/app/services/merchantinfo.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
-import {MerchantInfo, Tag} from '../models';
+import {Merchant, MerchantInfo, Tag} from '../models';
 import {AuthorizationService} from './authorization.service';
 
 @Injectable()
@@ -15,7 +15,7 @@ export class MerchantInfoService {
     }
 
     getAllTags(): Observable<Tag[]> {
-        return this.http.get<MerchantInfo[]>('/api/tag');
+        return this.http.get<Tag[]>('/api/tag');
     }
 
     getAll(): Observable<MerchantInfo[]> {
@@ -33,14 +33,14 @@ export class MerchantInfoService {
             return Observable.of(this.merchantInfos);
     }
 
-    create(merchant: MerchantInfo) {
+    create(merchant: MerchantInfo): Observable<Merchant> {
         const httpOptions = {
             headers: new HttpHeaders({
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + this.authorizationService.getToken()
             })
         };
-        return this.http.post('api/merchant/account', merchant, httpOptions).map((success) => {
+        return this.http.post<Merchant>('api/merchant/account', merchant, httpOptions).map((success) => {
             if (this.merchantInfos && this.merchantInfos.length > 0) {
                 this.merchantInfos.push(merchant);
             }
