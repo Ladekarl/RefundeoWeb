1512d75b0d85a3cfd1afe0ab1760fa1ee9f5dc65
nielsladekarl <nielsladekarl@gmail.com>
2018-08-31 23:59:03 -0700

    Cities

---
+++ Migrations/20180901020014_CityLocation.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Refundeo.Migrations
+{
+    public partial class CityLocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "LocationId",
+                table: "Cities",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_LocationId",
+                table: "Cities",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cities_Locations_LocationId",
+                table: "Cities",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cities_Locations_LocationId",
+                table: "Cities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_LocationId",
+                table: "Cities");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Cities");
+        }
+    }
+}
